98df2bd6b6d263b94589876785f6292659a72d30
Aung KO <aungko.uit@gmail.com>
2023-08-12 01:25:26 +0630

    feat: 🎉 add to do lists feature

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import {
+  Button,
+  Text,
+  Input,
+  Group,
+  Stack,
+  Checkbox,
+  CloseButton,
+} from "@mantine/core";
+import "./App.css";
+import { useRef, useState } from "react";
+function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Project",
+      complete: false,
+    },
+    {
+      id: 2,
+      title: "Finish Presentation",
+      complete: false,
+    },
+  ]);
+
+  // add item
+  const onAddHandle = () => {
+    if (inputRef.current?.value) {
+      setTodos([
+        ...todos,
+        {
+          id: Math.random(),
+          title: inputRef.current.value,
+          complete: false,
+        },
+      ]);
+    }
+  };
+
+  // add item by enter
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onAddHandle();
+    }
+  };
+
+  // delete item
+  const deleteItem = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <Stack p={24}>
+      <Group>
+        <Input
+          ref={inputRef}
+          placeholder="Enter your tasks"
+          onKeyDown={handleKeyPress}
+        />
+        <Button radius={"xl"} onClick={onAddHandle}>
+          Add
+        </Button>
+      </Group>
+      <Text fz={48} fw={"bold"}>
+        To do lists:
+      </Text>
+      <Stack spacing={3}>
+        {todos.map((todo) => (
+          <Group position="apart">
+            <Checkbox
+              key={todo.id}
+              label={todo.title}
+              size={"md"}
+              style={{
+                textDecoration: todo.complete ? "line-through" : "none",
+              }}
+              onClick={() => toggleComplete(todo.id)}
+            />
+
+            <CloseButton
+              title="Close popover"
+              size="xl"
+              iconSize={20}
+              color="red"
+              variant="transparent"
+              onClick={() => deleteItem(todo.id)}
+            />
+          </Group>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
+
+export default App;
